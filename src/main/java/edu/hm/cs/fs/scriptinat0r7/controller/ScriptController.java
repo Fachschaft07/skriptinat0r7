84771f752dae3863ba45c66c6861dfe07d065d0d
Maximilian Götz <goetz1@hm.edu>
2014-08-27 22:17:21 +0200

    Implemented first handler method in ScriptController and wrote a test for it.

--- src/main/java/edu/hm/cs/fs/scriptinat0r7/controller/ScriptController.java
+++ src/main/java/edu/hm/cs/fs/scriptinat0r7/controller/ScriptController.java
@@ -2,8 +2,11 @@ package edu.hm.cs.fs.scriptinat0r7.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import edu.hm.cs.fs.scriptinat0r7.model.Script;
 import edu.hm.cs.fs.scriptinat0r7.repositories.ScriptRepository;
 
 /**
@@ -15,5 +18,12 @@ public class ScriptController {
 
     @Autowired
     private ScriptRepository scripts;
-
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public String getAllScripts(ModelMap model) {
+        Iterable<Script> allScripts = scripts.findAll();
+        model.addAttribute("scripts", allScripts);
+        
+        return "allScripts";
+    }
 }
--- src/test/java/edu/hm/cs/fs/scriptinat0r7/controller/ScriptControllerTest.java
+++ src/test/java/edu/hm/cs/fs/scriptinat0r7/controller/ScriptControllerTest.java
@@ -3,6 +3,18 @@
  */
 package edu.hm.cs.fs.scriptinat0r7.controller;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Collections;
+
+import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.ui.ModelMap;
+
+import edu.hm.cs.fs.scriptinat0r7.model.Script;
+import edu.hm.cs.fs.scriptinat0r7.repositories.ScriptRepository;
+
 /**
  * Test class for {@link edu.hm.cs.fs.scriptinat0r7.controller.ScriptController}.
  *
@@ -10,5 +22,21 @@ package edu.hm.cs.fs.scriptinat0r7.controller;
  *
  */
 public class ScriptControllerTest {
+    
+    @Test
+    public void testGetAllScripts() throws Exception {
+        ScriptController controller = new ScriptController();
+        ScriptRepository repo = mock(ScriptRepository.class);
+        ModelMap model = mock(ModelMap.class);
+        ReflectionTestUtils.setField(controller, "scripts", repo);
+        Iterable<Script> scripts = Collections.emptyList();
+        when(repo.findAll()).thenReturn(scripts);
+        
+        String viewName = controller.getAllScripts(model);
+        
+        verify(repo).findAll();
+        verify(model).addAttribute("scripts", scripts);
+        assertEquals("The returned view name is not correct", "allScripts", viewName);
+    }
 
 }
