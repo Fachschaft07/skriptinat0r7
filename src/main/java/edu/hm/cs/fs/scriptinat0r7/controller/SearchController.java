70615378f582f4b7375d8a1d531993b45bd7d4b4
Fabian Trampusch <fabian.trampusch@freenet.de>
2014-08-28 12:38:28 +0200

    mysql works, added search for professors

--- src/main/java/edu/hm/cs/fs/scriptinat0r7/controller/SearchController.java
+++ src/main/java/edu/hm/cs/fs/scriptinat0r7/controller/SearchController.java
@@ -4,23 +4,39 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import edu.hm.cs.fs.scriptinat0r7.model.Professor;
+import edu.hm.cs.fs.scriptinat0r7.model.Script;
+import edu.hm.cs.fs.scriptinat0r7.repositories.ProfessorRepository;
+import edu.hm.cs.fs.scriptinat0r7.repositories.ScriptRepository;
+
 @Controller
 @RequestMapping("/search")
 public class SearchController {
 
+    @Autowired
+    private ScriptRepository scripts;
+    
+    @Autowired
+    private ProfessorRepository professors;
+    
     @RequestMapping
     public @ResponseBody Collection<SearchResult> index(final ModelMap model, @RequestParam("q") String searchQuery) {
         List<SearchResult> result = new ArrayList<>();
-        result.add(new SearchResult(searchQuery, "http://salatkuchen.net"));
-        result.add(new SearchResult("Hans Peter 1", "http://salatkuchen1.net"));
-        result.add(new SearchResult("Hans Peter 2", "http://salatkuchen2.net"));
-        result.add(new SearchResult("Hans Peter 3", "http://salatkuchen3.net"));
+        
+        for(Script script : scripts.findByNameContaining(searchQuery)) {
+            result.add(new SearchResult(script.toString(), script.toString()));
+        }
+        for(Professor professor : professors.findByFirstNameContainingOrLastNameContaining(searchQuery)) {
+            result.add(new SearchResult(professor.toString(), professor.toString()));
+        }
+        
         return result;
     }
 
--- src/main/java/edu/hm/cs/fs/scriptinat0r7/repositories/ProfessorRepository.java
+++ src/main/java/edu/hm/cs/fs/scriptinat0r7/repositories/ProfessorRepository.java
@@ -2,10 +2,14 @@ package edu.hm.cs.fs.scriptinat0r7.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
 import edu.hm.cs.fs.scriptinat0r7.model.Professor;
 
-public interface ProfessorRepository {
+public interface ProfessorRepository extends PagingAndSortingRepository<Professor, Long> {
 
-    List<Professor> findByFirstNameContainingOrLastNameContaining(String name);
-
+    @Query("select u from #{#entityName} u where u.firstName like %:name% or u.lastName like %:name%")
+    List<Professor> findByFirstNameContainingOrLastNameContaining(@Param("name") String name);
 }
